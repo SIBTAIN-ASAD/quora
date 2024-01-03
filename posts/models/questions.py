3cba8341192c5cebbe026ded4c7cdc9f48604080
Muhammad Sibtain Asad <sibtain.asad@devsinc.com>
2024-01-03 14:03:12 +0500

    add : models - added models and make packages for layers in project

---
+++ posts/models/questions.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    '''
+    Question model
+    '''
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE)
+    topics = models.ManyToManyField("Topic")
---
+++ posts/models/topics.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .questions import Question
+
+# Create your models here.
+
+class Topic(models.Model):
+    '''
+    Topic model
+    '''
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    questions = models.ManyToManyField(Question)
+    author = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE)
